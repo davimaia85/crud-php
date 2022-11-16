aaf1078a6a74f50e8a34eb95d9f23e9e09ebfd11
SouDev01 <davilnd8@gmail.com>
2022-11-16 11:56:42 -0300

    editando controller

--- src/controller/alunoController.php
+++ src/controller/alunoController.php
@@ -21,11 +21,19 @@ function inicio() : void //void = funcao sem retorno
 
 function listar() : void
 {
+    $select = abrirConexao()->query("SELECT * FROM tb_bebidas");
     include '../src/views/listar.phtml';
 }
 
 function novo() : void
-{
+{   
+    if (false === empty($_POST)){
+        $nome = $_POST['nome'];
+        $quantidade = $_POST['quantidade'];
+        $select = "INSERT INTO tb_bebidas (nome, quantidade) VALUES '{$nome}', '{$quantidade}';
+        $query = abrirConexao()->prepare($select);
+        $query->execute;
+    }
     include '../src/views/novo.phtml';
 }
 function editar() : void
